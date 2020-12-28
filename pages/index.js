1deb073f03dec8eb0d209fc862f9aa71e8f87b87
Dioni Alves <Dioni Alves>
2020-12-27 22:04:06 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+function Home() {
+    return <div>
+                <h1>Pagina em Desenvolvimento</h1>
+                <div>Em breve pagina pessoal de Dioni Alves, Web Developer</div>
+            </div>
+}
+
+export default Home
