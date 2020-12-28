22d4d3f8cef687053a372562061aeb36cc3c56d2
Dioni Alves <Dioni Alves>
2020-12-28 10:09:03 -0300

    Adicionando contato

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     return <div>
                 <h1>Pagina em Desenvolvimento</h1>
                 <div>Em breve pagina pessoal de Dioni Alves, Web Developer</div>
+                <div>Contato: (49) 99964-0273</div>
             </div>
 }
 
